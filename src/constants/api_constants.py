434b5f8f6ad55c1a1a7f07f3ca6559e7fb9d0bc5
shubham-johri85 <shubham.johri85@gmail.com>
2025-01-06 07:46:14 +0530

    2nd Jan

---
+++ src/constants/api_constants.py
@@ -0,0 +1,15 @@
+"""
+API Constants - class which contains all the endpoints
+Keep all the URLs
+"""
+
+class APIConstants:
+    def base_url(self):
+        return "https://restful-booker.herokuapp.com"
+
+    def url_create_booking(self):
+        return "https://restful-booker.herokuapp.com/booking"
+
+    def url_create_token(self):
+        return "https://restful-booker.herokuapp.com/auth"
+
---
+++ src/tests/testcases.py
@@ -0,0 +1,7 @@
+import pytest
+import allure
+import requests
+
+@allure.title("Sample Test")
+def test_sample():
+    assert True==True
